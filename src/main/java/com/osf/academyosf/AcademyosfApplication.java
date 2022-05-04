56cc8bbeadc9c73494d79e971fd5911d62a4781c
Sandro Santos <sandro.ads.dev@gmail.com>
2022-05-04 19:58:43 -0300

    Criando o projeto academy

---
+++ src/main/java/com/osf/academyosf/AcademyosfApplication.java
@@ -0,0 +1,13 @@
+package com.osf.academyosf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyosfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyosfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osf/academyosf/AcademyosfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osf.academyosf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyosfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
